16a02a7a1765aa174bdc279b947b904345f05f8d
sh2rman2006 <sh2rman2006@gmail.com>
2023-12-18 22:02:55 +0300

    sd

--- 17/erfes.py
+++ 17/erfes.py
@@ -1,1 +1,4 @@
 
+a=filter(lambda x:x>69,[i for i in range(1,101)])
+print(a)
+
--- phyton+/2 ступень/тема 5.4 высшие функции/тема 5.4 номер 1.py
+++ phyton+/2 ступень/тема 5.4 высшие функции/тема 5.4 номер 1.py
@@ -1,3 +1,3 @@
 print(*filter(lambda r:r>0,map(int, input("-").split())))
 
-
+# у filter(функция,массив) lamda вход данные:что делает/запомнит

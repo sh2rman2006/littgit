8aa369a36408e495bf6e6e49ca7e6e7e22047e63
sh2rman2006 <sh2rman2006@gmail.com>
2023-12-19 20:31:56 +0300

    Update erfes.py

--- 17/erfes.py
+++ 17/erfes.py
@@ -1,2 +1,24 @@
 a=[123,2345,53,464,345,321,4235,3452345,45,1]
 print(*filter(lambda x:x>70,a))
+
+def od(n,s):
+    ms=0
+    for i in str(n):
+        ms+=int(i)
+    return ms
+def vt(n,s):
+    ms=1
+    for i in str(n):
+        ms*=int(i)
+    return ms
+def f(n,s):
+    if n==s:
+        return 1
+    if n>s:
+        return 0
+    return od(n,s)+vt(n,s)
+count=0
+for i in range(10,101):
+    if f(i,8):
+        count+=1
+print(count)

cccf5950cbc850934247a06e3c36491ef3322a91
sh2rman2006 <sh2rman2006@gmail.com>
2023-12-27 17:25:27 +0300

    sd

--- рекурсия (16)/16.shk.py
+++ рекурсия (16)/16.shk.py
@@ -1,24 +1,30 @@
 from sys import *
 setrecursionlimit(10000)
-# def g(n):
-#     if n<2:
-#         return n
-#     if n>=2:
-#         return g(n//2)+g(n%2)
-# count=0
-# for i in range(100000):
-#     if g(i)==27:
-#         print(g(i))
+def g(n):
+    if n<2:
+        return n
+    if n>=2:
+        return a[n//2]+a[n%2]
+
+a=[0]*(100001)
+count=0
+for i in range(1,100001):
+    a[i]=g(i)
+for i in range(1,100001):
+    print(a[i])
+#     if a[i]==27:
+#         count+=1
+# print(count)
 
 
-def f(n):
-    if n==1:
-        return 1
-    if n==2:
-        return 3
-    if n>2:
-        return a[n-1]*n+a[n-2]*(n-1)
-a=[0]*6
-for i in range(1,6):
-    a[i]=f(i)
-print(a[5])
+# def f(n):
+#     if n==1:
+#         return 1
+#     if n==2:
+#         return 3
+#     if n>2:
+#         return a[n-1]*n+a[n-2]*(n-1)
+# a=[0]*6
+# for i in range(1,6):
+#     a[i]=f(i)
+# print(a[5])

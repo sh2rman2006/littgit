a88b7e851a35dd53755023f74e11edd868d36090
sh2rman2006 <sh2rman2006@gmail.com>
2024-01-05 00:45:31 +0300

    sd

---
+++ эйлер.py
@@ -0,0 +1,17 @@
+#10
+from math import *
+# sm=0
+# for i in range(2,2000001):
+#     count=0
+#     for d in range(2,round(i**(1/2))+1):
+#         if i**(1/2)%1==0:
+#             count+=1
+#         elif i%d==0:
+#             count+=2
+#     if count==0:
+#         sm+=i
+# print(sm)
+#готово
+
+
+

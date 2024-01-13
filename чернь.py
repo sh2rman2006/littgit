481846f6189e830fecd96f9cf82316c53d84fb4e
sh2rman2006 <sh2rman2006@gmail.com>
2024-01-13 13:10:26 +0300

    sd

--- чернь.py
+++ чернь.py
@@ -1,6 +1,11 @@
-
-
-
-
-
-
+for i in range(1,10**9):
+    count=0
+    for d in range(1,int(i**0.5)):
+        if i%d==0:
+            count+=2
+    if i**0.5%1==0:
+        count+=1
+    if count < 125:
+        continue
+    elif len(str(count))==3 and count**(1/3)%1==0:
+        print(i)

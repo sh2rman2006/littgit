dd6f82f0b2456ea2f5a81f81e2838a5086ad80b2
sh2rman2006 <sh2rman2006@gmail.com>
2024-01-15 14:34:13 +0300

    sd

--- чернь.py
+++ чернь.py
@@ -1,11 +1,29 @@
-for i in range(1,10**9):
+# for i in range(1,10**9):
+#     count=0
+#     for d in range(2,int(i**0.5)):
+#         if i%d==0:
+#             count+=2
+#     if i**0.5%1==0:
+#         count+=1
+#     if count < 125:
+#         continue
+#     elif len(str(count))==3 and count**(1/3)%1==0:
+#         print(i)
+
+
+for i in range(1273547,1273547*5):
+    m=0
     count=0
-    for d in range(1,int(i**0.5)):
+    for d in range(2,int(i**0.5)+1):
         if i%d==0:
-            count+=2
+            m=m+d+i//d
     if i**0.5%1==0:
-        count+=1
-    if count < 125:
-        continue
-    elif len(str(count))==3 and count**(1/3)%1==0:
-        print(i)
+        m-=int(i**0.5)
+    ost=m%100000
+    for ostdel in range(2,int(i**0.5)):
+        if i%ostdel==0:
+            count+=2
+        if ost**0.5%1==0:
+            count+=1
+
+

9e9facfe2a4565ac2baf55c0015daf88d7a811f0
sh2rman2006 <sh2rman2006@gmail.com>
2024-01-15 21:03:45 +0300

    sd

--- чернь 2.py
+++ чернь 2.py
@@ -1,3 +1,14 @@
+# for i in range(2,17):
+#     m=0
+#     count=0
+#     for d in range(2,int(i**0.5)+1):
+#         if i%d==0:
+#             m=m+d+i//d
+#     if i**0.5%1==0:
+#         m-=int(i**0.5)
+#     print(i,m)
+
+
 for i in range(2,17):
     m=0
     count=0
@@ -6,4 +17,23 @@ for i in range(2,17):
             m=m+d+i//d
     if i**0.5%1==0:
         m-=int(i**0.5)
-    print(i,m)
+    ostat=m%100000
+    if m>0:
+        for ostdel in range(2,int(ostat**0.5)+1):
+            if ostat%ostdel==0:
+                break
+            if ostat**0.5%1==0:
+                break
+        else:
+            print(i,m)
+
+        # for ostdel in range(2,int(ostat**0.5)+1):
+        #     if ostat%ostdel==0:
+        #         count+=2
+        #     if ostat**0.5%1==0:
+        #         count+=1
+        #     if count>0:
+        #         break
+        # else:
+        #     print(i,m)
+print(2%100000)
--- чернь.py
+++ чернь.py
@@ -1,16 +1,3 @@
-# for i in range(1,10**9):
-#     count=0
-#     for d in range(2,int(i**0.5)):
-#         if i%d==0:
-#             count+=2
-#     if i**0.5%1==0:
-#         count+=1
-#     if count < 125:
-#         continue
-#     elif len(str(count))==3 and count**(1/3)%1==0:
-#         print(i)
-
-
 for i in range(1273547,1273547*5):
     m=0
     count=0
@@ -20,13 +7,14 @@ for i in range(1273547,1273547*5):
     if i**0.5%1==0:
         m-=int(i**0.5)
     ost=m%100000
-    for ostdel in range(2,int(i**0.5)):
-        if m%ostdel==0:
-            break
-        if ost ** 0.5 % 1 == 0:
-            break
-    else:
-        print(i,m)
+    if m>0:
+        for ostdel in range(2,int(m**0.5)+1):
+            if ost%ostdel==0:
+                break
+            if ost ** 0.5 % 1 == 0:
+                break
+        else:
+            print(i,m)
 
 
 

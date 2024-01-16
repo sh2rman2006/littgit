93cab184d9a74f04f0cb2d62daace2611642894d
sh2rman2006 <sh2rman2006@gmail.com>
2024-01-16 18:53:01 +0300

    sd

--- чернь.py
+++ чернь.py
@@ -1,6 +1,17 @@
+b=[str(i**3) for i in range(5,10)]
+print(b)
 
-
-
-
+for i in range(10**9+1,10**8,-1):
+    count=0
+    m=[]
+    for d in range(2,int(i**0.5)):
+        if i%d==0:
+            count+=2
+            m.append(i//d)
+    if i**0.5%1==0:
+        count+=1
+    count+=2
+    if str(count) in b:
+        print(i,max(m))
 
 

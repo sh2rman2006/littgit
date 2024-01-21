5fa52a7eca3e20a01aa1d2ad989ebfc350b38fc0
sh2rman2006 <sh2rman2006@gmail.com>
2024-01-21 21:13:56 +0300

    sd

--- варианты/kompege.py
+++ варианты/kompege.py
@@ -158,10 +158,10 @@
 # print(f(2,20))
 
 """25"""
-from fnmatch import *
-for i in range(2024,10**10+1,2024):
-    if fnmatch(str(i),"1?2157*4"):
-        print(i,i//2024)
+# from fnmatch import *
+# for i in range(2024,10**10+1,2024):
+#     if fnmatch(str(i),"1?2157*4"):
+        # print(i,i//2024)
 
 
 

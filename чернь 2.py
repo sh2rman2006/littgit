cc779396dcafc768e78dc3db4dd263498c3348f0
sh2rman2006 <sh2rman2006@gmail.com>
2024-01-23 16:25:35 +0300

    sd

--- чернь 2.py
+++ чернь 2.py
@@ -21,17 +21,41 @@
 
 
 
-a="124456943"
+# a="124456943"
+# print(len(a))
+# ma=0
+# for i in range(len(a)):
+#     if a[i]=="1":
+#         count=0
+#         for k in range(i+1,len(a)):
+#             if a[k]=="3":
+#                 ma=max(count,ma)
+#                 count=0
+#             else:
+#                 count+=1
+# print(ma)
+
+
+
+# a = [123123]
+# b = [131313]
+# print(*zip(a, b))
+
+
+
+a="124415694313434323451"
 print(len(a))
 ma=0
 for i in range(len(a)):
     if a[i]=="1":
         count=0
+        ln=0
         for k in range(i+1,len(a)):
-            if a[k]=="3":
-                ma=max(count,ma)
-                count=0
+            if a[k]=="1":
+                ln+=1
+                count+=1
             else:
                 count+=1
+            if ln==2:
+                ma=max(ma,count)
 print(ma)
-

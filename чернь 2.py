b2182f98c75345a6849ec8f1df329cda6c8e3072
sh2rman2006 <sh2rman2006@gmail.com>
2024-01-24 15:59:00 +0300

    sd

--- чернь 2.py
+++ чернь 2.py
@@ -1,10 +1,9 @@
-from ipaddress import *
-a=ip_network("192.168.64.176/255.255.255.240",False)
-count=0
-for i in a:
-    ln=bin(int(i))[2:]
-    if ln.count("1")%2==0:
-        count+=1
-print(count)
-
+# from ipaddress import *
+# a=ip_network("192.168.64.176/255.255.255.248",False)
+# count=0
+# for i in a:
+#     ln=bin(int(i))[2:]
+#     if ln.count("1")%2!=0:
+#         count+=1
+# print(count)
 

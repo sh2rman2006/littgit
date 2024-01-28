8d5bd1204bba36e3f73ce9fcc5ca0e2b831a160f
sh2rman2006 <sh2rman2006@gmail.com>
2024-01-28 17:04:09 +0300

    sd

--- 13/egkr.py
+++ 13/egkr.py
@@ -3,6 +3,7 @@ a=ip_network("192.168.32.48/255.255.255.240",False)
 count=0
 for i in a:
     c=bin(int(i))[2:]
+    print(c,a)
     if c.count("1")%2!=0:
         count+=1
 print(count)
--- чернь 2.py
+++ чернь 2.py
@@ -1,10 +1,23 @@
-# from ipaddress import *
-# a=ip_network("192.168.64.176/255.255.255.248",False)
+from ipaddress import *
+# a=ip_network("172.118.1.255/255.255.252.0",False)
 # count=0
 # for i in a:
-#     ln=bin(int(i))[2:]
-#     if ln.count("1")%2!=0:
+#     c=bin(int(i))[2:]
+#     sum=c.count("1")
+#     for d in range(2,int(sum**0.5)+1):
+#         if sum%d==0:
+#             break
+#     else:
 #         count+=1
 # print(count)
+"""2"""
+a=ip_network('49.26.38.163/255.255.255.224',False)
+for i in a:
+    c=bin(int(i))[2:]
+
+
+
+
+
 
 

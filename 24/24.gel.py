b12b22f6f4f9f7ba5e5d6b41e436ed168de21f13
sh2rman2006 <sh2rman2006@gmail.com>
2024-02-04 23:08:30 +0300

    sd

--- 24/24.gel.py
+++ 24/24.gel.py
@@ -1,12 +1,12 @@
 file = open('24.gel.txt').readline()
 ch = [str(i) for i in range(10)]
-sim = 'AEIQUY'
+sim = 'AEIOUY'
 count = 0
 maxc = 0
 
 def ysl(i,b):
     i += 3
-    if ((file[i] in ch) and ( file[i+1] in ch) and file[i+2] in sim):
+    if ((file[i] in ch) and ( file[i+1] in ch)) and (file[i+2] not in sim):
         if int(file[i] + file[i+1])%2 != 0:
             b += 1
             return ysl(i,b)
@@ -17,7 +17,7 @@ def ysl(i,b):
 
 
 for i in range(len(file)-2):
-    if ((file[i] in ch) and (file[i+1] in ch)) and (file[i+2] in sim):
+    if ((file[i] in ch) and (file[i+1] in ch)) and (file[i+2] not in sim):
         if int(file[i] + file[i+1])%2 != 0:
             count =ysl(i, 1)
     else:
---
+++ 24/24.vrot.py
@@ -0,0 +1,33 @@
+from itertools import *
+a=product("abcdefu".upper(),repeat=3)
+sogl="BCDF"
+gl="AEU"
+file =open("24.vrot.txt").readline()
+
+komb=["".join(i) for i in a if (i[0] in sogl) and (i[1] in gl) and (i[2] in sogl)]
+print(komb)
+
+count=0
+maxc=0
+
+for i in range(len(file)-2):
+    if file[i]+file[i+1]+file[i+2] in komb:
+        for k in range(i+1,len(file)-2):
+            v=1
+            count=2
+            if file[k]+file[k+1]+file[k+2] in komb:
+                v+=1
+                count+=1
+            if v==2:
+                maxc=max(count,maxc)
+                count=2
+                break
+            else:
+                count+=1
+print(maxc)
+
+
+
+
+
+

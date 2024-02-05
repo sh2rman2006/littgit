74076413144a348cd07156217533eaf48e3a16be
sh2rman2006 <sh2rman2006@gmail.com>
2024-02-06 01:17:16 +0300

    sd

--- 9/9.145.py
+++ 9/9.145.py
@@ -16,7 +16,7 @@ for i in open("9.145.csv"):
         ch1 = sum(ch) / len(ch)
     if len(nch) > 0:
         nch1 = sum(nch) / len(nch)
-    if ((len(rep)==2 and len(nrp)==4) and(not abs(ch1 - nch1) > 50)) or ((not(len(rep)==2 and len(nrp)==4) )and(abs(ch1 - nch1) > 50)):
+    if ((len(rep)==2 and len(nrp)==4) and(not abs(ch1 - nch1) > 50)) or ((not(len(rep)==2 and len(nrp)==4))and(abs(ch1-nch1)>50)):
         count+=1
 print(count)
 

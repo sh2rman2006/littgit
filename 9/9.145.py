d0f1155cdfcd25077dbd60716687670309cedf6b
sh2rman2006 <sh2rman2006@gmail.com>
2024-02-06 01:14:49 +0300

    sd

--- 9/9.145.py
+++ 9/9.145.py
@@ -3,22 +3,22 @@ for i in open("9.145.csv"):
     a=list(map(int,i.split(";")))
     rep=[i for i in a if a.count(i)==2]
     nrp=[i for i in a if a.count(i)==1]
-    if len(rep)==2 and len(nrp)==4:
-        ch=[]
-        nch=[]
-        for k in a :
-            if k%2==0:
-                ch.append(k)
-            if k%2!=0:
-                nch.append(k)
-        ch1=0
-        nch1=0
-        if len(ch)>0:
-            ch1 = sum(ch) / len(ch)
-        if len(nch) > 0:
-            nch1 = sum(nch) / len(nch)
-        if abs(ch1-nch1)>50:
-            count+=1
+    ch = []
+    nch = []
+    for k in a:
+        if k % 2 == 0:
+            ch.append(k)
+        if k % 2 != 0:
+            nch.append(k)
+    ch1 = 0
+    nch1 = 0
+    if len(ch) > 0:
+        ch1 = sum(ch) / len(ch)
+    if len(nch) > 0:
+        nch1 = sum(nch) / len(nch)
+    if ((len(rep)==2 and len(nrp)==4) and(not abs(ch1 - nch1) > 50)) or ((len(rep)==2 and len(nrp)!=4) and(abs(ch1 - nch1) > 50)):
+        count+=1
+
 print(count)
 
 

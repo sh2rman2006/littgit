acc40b38786508a4159ff71f05269a8c000fe04a
sh2rman2006 <sh2rman2006@gmail.com>
2024-02-17 17:01:02 +0300

    sd

--- чернь.py
+++ чернь.py
@@ -1,21 +1,23 @@
-a=[str(i.replace("\n",'')) for i in open('8 60 1 40.txt')]
-a=''.join(a)
-sm=0
-for i in range(len(a)):
-    if a[i]=='1':
-        c1=0
-        c2=0
-        for k in range(i+1,len(a)):
-            if a[k]=='0':
-                c1+=1
-                break
-            else:
-                c1+=1
-        for l in range(i,0,-1):
-            if a[l] == '0':
-                c2 += 1
-                break
-            else:
-                c2 += 1
-        sm+=min(c1,c2)
-print(sm)
+# a=[str(i.replace("\n",'')) for i in open('8 60 1 40.txt')]
+# a=''.join(a)
+# sm=0
+# for i in range(len(a)):
+#     if a[i]=='1':
+#         c1=0
+#         c2=0
+#         for k in range(i+1,len(a)):
+#             if a[k]=='0':
+#                 c1+=1
+#                 break
+#             else:
+#                 c1+=1
+#         for l in range(i,0,-1):
+#             if a[l] == '0':
+#                 c2 += 1
+#                 break
+#             else:
+#                 c2 += 1
+#         sm+=min(c1,c2)
+# print(sm)
+for i in range(10,0,-1):
+    print(i)

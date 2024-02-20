84f307734187899da82111306ced8e849c38002c
sh2rman2006 <sh2rman2006@gmail.com>
2024-02-20 18:45:23 +0300

    sd

--- 27/27_shastin.py
+++ 27/27_shastin.py
@@ -1,21 +1,22 @@
-a=[str(i.replace("\n",'')) for i in open('27A_shastin.txt')]
-a=''.join(a)
+file=[int(i) for i in open('27A_shastin.txt')]
 sm=0
-for i in range(len(a)):
-    if a[i]=='1':
+for i in range(len(file)):
+    if str(file[i])=='1':
         c1=0
-        c2=0
-        for k in range(i+1,len(a)):
-            if a[k]=='0':
+        print(i,'////')
+        for k in range(i+1,len(file)):
+            if file[k]=='0':
                 c1+=1
                 break
             else:
                 c1+=1
-        for l in range(i,0,-1):
-            if a[l] == '0':
-                c2 += 1
+
+        c2 = 0
+        for k in range(i-1,-1,-1):
+            if file[k]=='0':
+                c2+=1
                 break
             else:
-                c2 += 1
+                c2+=1
         sm+=min(c1,c2)
 print(sm)
---
+++ чернь.py
@@ -0,0 +1,21 @@
+file=[int(i) for i in open('8 60 1 40.txt')]
+print(file)
+sm=0
+for i in range(len(file)):
+    if str(file[i])=='1':
+        c1=0
+        for k in range(i+1,len(file)):
+            if file[k]=='0':
+                c1+=1
+                break
+            else:
+                c1+=1
+        c2 = 0
+        for k in range(i-1,-1,-1):
+            if file[k]=='0':
+                c2+=1
+                break
+            else:
+                c2+=1
+        sm+=min(c1,c2)
+print(sm,'sm')

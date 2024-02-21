781826d2758101b9fbea1312beb16059fe148433
sh2rman2006 <sh2rman2006@gmail.com>
2024-02-21 21:02:36 +0300

    sd

--- 27/27_shastin.py
+++ 27/27_shastin.py
@@ -1,22 +1,28 @@
-file=[int(i) for i in open('27A_shastin.txt')]
+file=[int(i) for i in open('27A_shast.txt')]
 sm=0
+print(file,len(file))
 for i in range(len(file)):
     if str(file[i])=='1':
         c1=0
-        print(i,'////')
         for k in range(i+1,len(file)):
-            if file[k]=='0':
+            if str(file[k])=='0':
                 c1+=1
                 break
             else:
                 c1+=1
-
         c2 = 0
         for k in range(i-1,-1,-1):
-            if file[k]=='0':
+            if str(file[k])=='0':
                 c2+=1
                 break
             else:
                 c2+=1
-        sm+=min(c1,c2)
-print(sm)
+        if c1==0:
+            sm+=c2
+            print(i)
+        if c2==0:
+            sm+=c1
+            print(i)
+        if c1>0 and c2>0:
+            sm+=min(c1,c2)
+print(sm,'sm')
